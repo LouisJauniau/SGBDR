0547de0cd5b63e97d79bddcb9378884fe3d6c9f0
LouisJauniau <159655673+LouisJauniau@users.noreply.github.com>
2024-09-26 17:33:30 +0200

    Add files via upload

---
+++ PageId.java
@@ -0,0 +1,19 @@
+package up.mi.jgm.td1;
+
+public class PageId {
+    private int FileIdx;
+    private int PageIdx;
+
+    public PageId(int FileIdx, int PageIdx) {
+        this.FileIdx = FileIdx;
+        this.PageIdx = PageIdx;
+    }
+
+    public int getFileIdx() {
+        return FileIdx;
+    }
+
+    public int getPageIdx() {
+        return PageIdx;
+    }
+}
