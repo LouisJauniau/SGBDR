ae8c425a714575a313623059634e80240cbec825
LouisJauniau <159655673+LouisJauniau@users.noreply.github.com>
2024-09-29 15:47:34 +0200

    Add files via upload

--- PageId.java
+++ PageId.java
@@ -4,16 +4,35 @@ public class PageId {
     private int FileIdx;
     private int PageIdx;
 
-    public PageId(int FileIdx, int PageIdx) {
-        this.FileIdx = FileIdx;
-        this.PageIdx = PageIdx;
+    // Constructeur qui prend en argument l'identifiant du fichier et l'indice de la page
+    public PageId(int fileIdx, int pageIdx) {
+        this.FileIdx = fileIdx;
+        this.PageIdx = pageIdx;
     }
 
+    // Getters et setters pour FileIdx et PageIdx
     public int getFileIdx() {
         return FileIdx;
     }
 
+    public void setFileIdx(int fileIdx) {
+        this.FileIdx = fileIdx;
+    }
+
     public int getPageIdx() {
         return PageIdx;
+    }
+
+    public void setPageIdx(int pageIdx) {
+        this.PageIdx = pageIdx;
+    }
+
+    // Méthode pour afficher le PageId sous forme de chaîne de caractères
+    @Override
+    public String toString() {
+        return "PageId{" +
+                "FileIdx=" + FileIdx +
+                ", PageIdx=" + PageIdx +
+                '}';
     }
 }
