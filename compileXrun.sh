31d97b4e01e58abd38a56b4036428d05608025c1
LouisJauniau <159655673+LouisJauniau@users.noreply.github.com>
2024-12-16 15:30:38 +0100

    modif fichier compilation

--- compileXrun.sh
+++ compileXrun.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-javac -cp "/Users/louisjauniau/Downloads/json-20240303.jar" $(find /Users/louisjauniau/eclipse-workspace/BDDA/SGBDR/src/up/mi/jgm/bdda/ -name "*.java")
-java -cp "/Users/louisjauniau/eclipse-workspace/BDDA/SGBDR/src:/Users/louisjauniau/Downloads/json-20240303.jar" up.mi.jgm.bdda.SGBD "/Users/louisjauniau/eclipse-workspace/BDDA/SGBDR/config.json"
+# Définir le répertoire du projet à partir du script (répertoire où se trouve le script)
+SCRIPT_DIR=$(dirname "$0")
+PROJECT_DIR=$(realpath "$SCRIPT_DIR/..")  # Chemin de base
+# Chemins par défaut
+LIB_DIR="${LIB_DIR:-$PROJECT_DIR/SGBDR}"
+SRC_DIR="${SRC_DIR:-$PROJECT_DIR/SGBDR/src}" 
+CONFIG_FILE="${CONFIG_FILE:-$PROJECT_DIR/SGBDR/config.json}"
+
+# Compilation
+javac -cp "$LIB_DIR/json-20240303.jar" $(find "$SRC_DIR" -name "*.java")
+
+# Exécution
+java -cp "$SRC_DIR:$LIB_DIR/json-20240303.jar" up.mi.jgm.bdda.SGBD "$CONFIG_FILE"
