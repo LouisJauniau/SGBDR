1713153623f1276b43177bd6383d212d862990ff
LouisJauniau <159655673+LouisJauniau@users.noreply.github.com>
2024-12-19 11:49:57 +0100

    Update SelectCommandParser.java

--- src/up/mi/jgm/bdda/SelectCommandParser.java
+++ src/up/mi/jgm/bdda/SelectCommandParser.java
@@ -194,7 +194,6 @@ public class SelectCommandParser {
         if (condStr.contains("=")) return "=";
         if (condStr.contains("<")) return "<";
         if (condStr.contains(">")) return ">";
-        if (condStr.contains("!=")) return "!=";
         return null;
     }
 
